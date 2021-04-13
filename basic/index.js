5acec1555460711f479558cb3b0db3dd25de18c1
PEEWEE\petra <petravos70@gmail.com>
2021-04-13 19:41:53 +0200

    Create a first scene with a cube(w.i.p)

--- basic/index.js
+++ basic/index.js
@@ -1,1 +1,30 @@
-console.log(THREE);
+const scene = new THREE.Scene();
+
+// Create a red cube
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+const mesh = new THREE.Mesh(geometry, material);
+scene.add(mesh);
+
+// Sizes
+const sizes = {
+  width: 800,
+  height: 600,
+};
+
+// Provide a camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
+// move the camera backwards before the rendering => done with the z-position => forward or backwards
+camera.position.z = 3;
+// move the camera to the right
+camera.position.x = 1;
+scene.add(camera);
+
+// Render the scene
+const canvas = document.querySelector('.webgl');
+const renderer = new THREE.WebGLRenderer({
+  canvas: canvas,
+});
+renderer.setSize(sizes.width, sizes.height);
+
+renderer.render(scene, camera);
