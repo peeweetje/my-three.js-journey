e2524b9125f7966bf9d6313912b42a5642f1a948
PEEWEE\petra <petravos70@gmail.com>
2021-04-17 14:47:54 +0200

    Create more cubes with different positions.

--- basic/src/index.js
+++ basic/src/index.js
@@ -5,21 +5,31 @@ import './style.css';
 const scene = new THREE.Scene();
 
 // Object
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
-const mesh = new THREE.Mesh(geometry, material);
+const group = new THREE.Group();
+group.scale.y = 1;
+group.rotation.y = 0;
+scene.add(group);
 
-mesh.position.x = 0.7;
-mesh.position.y = -0.6;
-mesh.position.z = 1;
-scene.add(mesh);
+const cube1 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({ color: 0xff0000 })
+);
+cube1.position.x = -1.2;
+group.add(cube1);
 
-mesh.scale.x = 2;
-mesh.scale.y = 0.5;
-mesh.scale.z = 0.5;
+const cube2 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({ color: 0x00ff00 })
+);
+cube2.position.x = 1;
+group.add(cube2);
 
-mesh.rotation.x = Math.PI * 0.25;
-mesh.rotation.y = Math.PI * 0.25;
+const cube3 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({ color: 0x0000ff })
+);
+cube3.position.x = 3;
+group.add(cube3);
 
 /**
  * Axes Helper
