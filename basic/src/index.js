0203f37ec96eac5043a0399e5205d49de3fae724
PEEWEE\petra <petravos70@gmail.com>
2021-05-08 14:39:51 +0200

    Add OrbitControls for drag and drop functionality. Ad damping for smooth animation.

--- basic/src/index.js
+++ basic/src/index.js
@@ -1,5 +1,8 @@
 import './style.css';
 import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+
+console.log(OrbitControls);
 
 // Cursor
 const cursor = {
@@ -39,11 +42,13 @@ const camera = new THREE.PerspectiveCamera(
   100
 );
 
-// camera.position.x = 2;
-// camera.position.y = 2;
 camera.position.z = 3;
 camera.lookAt(mesh.position);
 scene.add(camera);
+
+// controls
+const controls = new OrbitControls(camera, canvas);
+controls.enableDamping = true;
 
 // Renderer
 const renderer = new THREE.WebGLRenderer({
@@ -61,10 +66,13 @@ const tick = () => {
   // mesh.rotation.y = elapsedTime;
 
   // update camera
-  camera.position.x = Math.sin(cursor.x * Math.PI * 2) * 2;
-  camera.position.z = Math.cos(cursor.x * Math.PI * 2) * 2;
-  camera.position.y = cursor.y * 3;
-  camera.lookAt(mesh.position);
+  // camera.position.x = Math.sin(cursor.x * Math.PI * 2) * 2;
+  // camera.position.z = Math.cos(cursor.x * Math.PI * 2) * 2;
+  // camera.position.y = cursor.y * 3;
+  // camera.lookAt(mesh.position);
+
+  // update controls
+  controls.update();
 
   // Render
   renderer.render(scene, camera);
