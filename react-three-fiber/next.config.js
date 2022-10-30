5f85d5d8d3372baacb9839764113295dbb4fa4b2
PEEWEE\petra <petravos70@gmail.com>
2022-10-30 11:18:20 +0100

    Add packages and config to support react-three-fiber.

--- react-three-fiber/next.config.js
+++ react-three-fiber/next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
+require('next-transpile-modules')(['three']);
+
 const nextConfig = {
+  withTM: true,
   reactStrictMode: true,
   swcMinify: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
